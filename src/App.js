3d4ed42ca58861b2cb5d137280321bce78b8d08e
darrenemir <101922827+darrenemir@users.noreply.github.com>
2022-03-21 19:55:21 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hi! Come ! Learn React
+          Learn React
         </a>
       </header>
     </div>
