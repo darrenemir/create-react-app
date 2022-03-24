c4e57a0d5fb85f2489966df98800c639f425a535
Dylan <dylankhiew@gmail.com>
2022-03-24 14:18:33 +0800

    update main screen component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,47 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [value, setValue] = useState('');
+
+  const onChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const onClick = () => {
+    const url = `https://www.mmu.edu.my/?s=${value}&post_type=courses`;
+    window.open(url, '_blank', 'noopener,noreferrer')
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div style={{ padding: '8px' }}>
+          <svg xmlns="http://www.w3.org/2000/svg" width="28" height="28" fill="currentColor" class="bi bi-chat-left-quote-fill" viewBox="0 0 16 16">
+            <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2H4.414a1 1 0 0 0-.707.293L.854 15.146A.5.5 0 0 1 0 14.793V2zm7.194 2.766a1.688 1.688 0 0 0-.227-.272 1.467 1.467 0 0 0-.469-.324l-.008-.004A1.785 1.785 0 0 0 5.734 4C4.776 4 4 4.746 4 5.667c0 .92.776 1.666 1.734 1.666.343 0 .662-.095.931-.26-.137.389-.39.804-.81 1.22a.405.405 0 0 0 .011.59c.173.16.447.155.614-.01 1.334-1.329 1.37-2.758.941-3.706a2.461 2.461 0 0 0-.227-.4zM11 7.073c-.136.389-.39.804-.81 1.22a.405.405 0 0 0 .012.59c.172.16.446.155.613-.01 1.334-1.329 1.37-2.758.942-3.706a2.466 2.466 0 0 0-.228-.4 1.686 1.686 0 0 0-.227-.273 1.466 1.466 0 0 0-.469-.324l-.008-.004A1.785 1.785 0 0 0 10.07 4c-.957 0-1.734.746-1.734 1.667 0 .92.777 1.666 1.734 1.666.343 0 .662-.095.931-.26z"/>
+          </svg>
+        </div>
+        <label style={{ fontWeight: 600, fontSize: '32px', marginBottom: '8px' }}>
+          Enrollment Agent
+        </label>
+        <text style={{ fontSize: '16px' }}>
+          Something is brewing real soon
+        </text>
+        <text style={{ marginTop: '32px', fontSize: '12px' }}>
+          In the meantime, search here
+        </text>
+        <input
+          type="text"
+          value={value}
+          onChange={onChange}
+          style={{ margin: '16px', borderRadius: '8px', background: 'white', padding: '12px', width: '300px' }}
+        />
+        <button
+          onClick={onClick}
+          style={{ width: '300px', height: '30px', borderRadius: '8px', fontSize: '10px', color: '#282c34', fontWeight: 600, backgroundColor: 'white' }}
         >
-          Learn React
-        </a>
+          SEARCH
+        </button>
       </header>
     </div>
   );
